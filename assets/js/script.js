97982ee2d0b951213ae1de75a5acf269f9255b93
Erik Dell <edellenator@gmail.com>
2022-04-01 21:13:02 -0700

    html refactor

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+const startBtn = document.querySelector (".start-btn");
+const rulesBox = document.querySelector(".rules");
+const btnBegin = rulesBox.querySelector(".btn .begin");
+const btnCancel = rulesBox.querySelector(".btn .cancel");
+const quizBox = document.querySelector(".quiz-box");
+const questionText = document.querySelector(".question-text");
+const options = document.querySelector(".options");
+const correct = document.querySelector(".correct");
+const incorrect = document.querySelector(".incorrect");
+const resultsBox = document.querySelector(".results-box");
+const playerName = document.querySelector("#player-name");
+const highScore = document.querySelector(".current-high-score");
+const playerScore = document.querySelector(".player-score");
+const timer = document.querySelector(".timer");
+
+// start button clicked
+
+startBtn.addEventListener ("click", console.log("you clicked it"))
+
