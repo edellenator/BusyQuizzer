a1c502668407d5795501e57d7a8326a80514318c
Erik Dell <edellenator@gmail.com>
2022-04-01 22:09:35 -0700

    start buttons begin quiz

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
-const startBtn = document.querySelector (".start-btn");
+const startBtn = document.querySelector (".start-btn button");
 const rulesBox = document.querySelector(".rules");
-const btnBegin = rulesBox.querySelector(".btn .begin");
-const btnCancel = rulesBox.querySelector(".btn .cancel");
+const btnBegin = rulesBox.querySelector(".begin");
+const btnCancel = rulesBox.querySelector(".cancel");
 const quizBox = document.querySelector(".quiz-box");
 const questionText = document.querySelector(".question-text");
 const options = document.querySelector(".options");
@@ -15,5 +15,23 @@ const timer = document.querySelector(".timer");
 
 // start button clicked
 
-startBtn.addEventListener ("click", console.log("you clicked it"))
+var pressStart = function() {
+    console.log("you pressed start");
+    rulesBox.classList.add("activeRules");
+};
 
+startBtn.addEventListener("click", pressStart)
+
+var pressCancel = function() {
+    console.log ("you pressed cancel");
+    rulesBox.classList.remove("activeRules");
+}
+
+btnCancel.addEventListener("click", pressCancel);
+
+var quizStart = function() {
+    console.log("you started the quiz")
+    quizBox.classList.add("activeQuiz");
+}
+
+btnBegin.addEventListener("click", quizStart);
