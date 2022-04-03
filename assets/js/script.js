30a817b443e582b62d4d6f70dfd30db1db8d1edc
Erik Dell <edellenator@gmail.com>
2022-04-02 22:18:26 -0700

    answers progess quiz. Deduct time for wrong, add points for correct

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,23 +5,18 @@ const btnCancel = rulesBox.querySelector(".cancel");
 const quizBox = document.querySelector(".quiz-box");
 const questionNumber = document.querySelector(".question-number")
 const questionText = document.querySelector(".question-text");
-const options = document.querySelector(".options");
+const optionList = document.querySelector(".options");
 const correct = document.querySelector(".correct");
 const incorrect = document.querySelector(".incorrect");
 const resultsBox = document.querySelector(".results-box");
-const playerName = document.querySelector("#player-name");
 const highScore = document.querySelector(".current-high-score");
 const playerScore = document.querySelector(".player-score");
 const timer = document.querySelector(".timer");
-var user = [
-    {
-    name:"",
-    score: 0,
-    }
-]
-var userLogs = [
+var userScore = 0;
+var currentHigh = 0;
+var userName = "";
+var userLogs = [];
 
-]
 
 var questions = [
     {
@@ -60,6 +55,7 @@ var questions = [
 ]
 var timeStart = 300
 
+
 var questionCount = 0;
 // start button clicked
 
@@ -79,11 +75,14 @@ btnCancel.addEventListener("click", pressCancel);
 
 var quizStart = function() {
     console.log("you started the quiz")
+    // reveal quiz box
     quizBox.classList.add("activeQuiz");
     rulesBox.classList.remove("activeRules")
+    // add questions to quiz box
+    showQuestions(questionCount)
     // start counter
-    showQuestions(0)
     startTimer(timeStart)
+    
 }
 
 var startTimer = function (counter) {
@@ -93,13 +92,12 @@ var startTimer = function (counter) {
         counter--;
         if (counter <= 0) {
             timer.textContent = 0
+            quizEnd();
         }
     }
 }
 
-var quizEnd = function() {
 
-}
 
 btnBegin.addEventListener("click", quizStart);
 
@@ -108,8 +106,90 @@ var showQuestions = function(index) {
     questionNumber.textContent = numb;
     var ask = questions[index].question;
     questionText.textContent = ask;
-    var optDisplay = "<li>" + questions[index].opt[0] + "</li>" + "<li>" + questions[index].opt[1] + "</li>" + "<li>" + questions[index].opt[2] + "</li>" + "<li>" + questions[index].opt[3] + "</li>";
-    options.innerHTML = optDisplay;                
+    var optDisplay = "<li class='option-item'>" + questions[index].opt[0] + "</li>" + "<li class='option-item'>" + questions[index].opt[1] + "</li>" + "<li class='option-item'>" + questions[index].opt[2] + "</li>" + "<li class='option-item'>" + questions[index].opt[3] + "</li>";
+    optionList.innerHTML = optDisplay;
+    
+    const optionItem = optionList.querySelectorAll(".option-item");
+
+    for (i=0; i < optionItem.length; i++) {
+        optionItem[i].setAttribute("onclick", "optionSelected(this)");
+    }
+    // const optionItem = optionList.querySelectorAll(".option-list")
+    // optionItem.addEventListener("click", optionSelected)
+    console.log(optionItem);
+
 }
 
+var optionSelected = function(select) {
+    var userSelect = select.textContent;
+    var correctAns = questions[questionCount].answer;
+    if (userSelect === correctAns) {
+        userScore = userScore + 10;
+        correct.classList.add("correctActive");
+        incorrect.classList.remove("incorrectActive"); 
+    }
+    else {
+        console.log(timer.textContent);
+        var decrementTime = timer.textContent;
+        decrementTime = decrementTime - 10;
+        clearInterval(timeStart);
+        timeStart = decrementTime;
+        startTimer(timeStart);
+        incorrect.classList.add("incorrectActive");
+        correct.classList.remove("correctActive");
+    }
+    console.log(userScore);
+    if (questionCount < questions.length - 1) {
+        questionCount++;
+        showQuestions(questionCount);
+    }
+    else {
+        quizEnd();
+    }
+    
+}
 
+var saveScores = function () {
+    localStorage.setItem("high score", currentHigh);
+    localStorage.setItem("User Scores", JSON.stringify(userLogs));
+    
+}
+
+var retrieveScores = function () {
+
+}
+
+var quizEnd = function() {
+    quizBox.classList.remove("activeQuiz");
+    correct.classList.remove("correctActive");
+    incorrect.classList.remove("incorrectActive");
+    resultsBox.classList.add("activeResults");
+    clearInterval(timeStart);
+    const submitName = document.querySelector(".submit-name")
+    playerScore.textContent="Your Score is " + userScore
+    highScore.textContent = " " + currentHigh;
+    submitName.onclick = ()=> {
+        console.log("you clicked it");
+        userName 
+        console.log(userName)
+        var userArray = [
+            {
+                name: userName = resultsBox.querySelector("input[name='player-name']").value,
+                score: userScore,
+            }
+        ];
+        userLogs.push(userArray);
+        console.log(userLogs)
+        if (userScore > currentHigh) {
+            const highScoreEl = document.querySelector(".high-scores");
+            var highScoreMes = document.createElement("p");
+            highScoreMes.textContent = "Congratulations, you have the new high score!";
+            highScoreEl.appendChild(highScoreMes);
+            currentHigh = userScore
+            console.log(currentHigh);
+        
+    };
+    saveScores();  
+    };
+   
+}
